ffdaf69eff101bd09b1f8384234012cec53b9dd0
james <james.speirs@gmail.com>
2018-09-19 17:23:57 -0600

    better expose typings for authorizedRequest

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ declare function byuOAuth (clientId: string, clientSecret: string): byuOAuth.Byu
 declare namespace byuOAuth {
 
     export interface ByuOAuth {
-        authorizedRequest (options: { body?: object|string, headers?: {[k: string]: string}, method?: string, query?: object, token?: ByuToken, url: string }): Promise<{body: string|object|void, headers: object, statusCode: number}>,
+        authorizedRequest (options: RequestWithTokenOptions): Promise<ResponseObject>,
         getAuthorizationUrl (redirectUri: string, scope?: string, state?: string): Promise<string>,
         getClientGrantToken (): Promise<ByuToken>,
         getCodeGrantToken (code: string, redirectUri: string, scope?: string): Promise<ByuToken>,
@@ -36,5 +36,28 @@ declare namespace byuOAuth {
         subjectTypesSupported: Array<string>,
         tokenEndpoint: string,
         userInfoEndpoint: string
+    }
+
+    export interface RequestOptions {
+        body?: object|string,
+        headers?: {[k: string]: string},
+        method?: string,
+        query?: object,
+        url: string
+    }
+
+    export interface RequestWithTokenOptions {
+        body?: object|string,
+        headers?: {[k: string]: string},
+        method?: string,
+        query?: object,
+        token?: ByuToken,
+        url: string
+    }
+
+    interface ResponseObject {
+        body: string|object|void,
+        headers: object,
+        statusCode: number
     }
 }
